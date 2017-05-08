cebf9e153427726b673fd572465776aebba7ba6d
Kenneth Baksteen <kenneth.baksteen@student.ucll.be>
2017-05-08 15:28:18 +0200

    Launcher aangepast voor punt en Punt.java toString()

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javax.swing.JOptionPane;
+import domain.*;
 
 import domain.Speler;
 
@@ -11,6 +12,11 @@ public class Launcher {
 		Speler speler = new Speler(naam);
 
 		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Geef de X-coördinaat"));
+		int y = Integer.parseInt(JOptionPane.showInputDialog("Geef de Y-coördinaat"));
+		Punt punt1 = new Punt(x, y);
+		JOptionPane.showMessageDialog(null, punt1);
 	}
 
 }
