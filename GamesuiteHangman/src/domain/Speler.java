bd355b1d288101ec1f10e5ee9dece014108ec800
DESKTOP-57INN8U\Michi <michieldewandelaer@hotmail.com>
2017-05-08 15:22:16 +0200

    commit speler

---
+++ GamesuiteHangman/src/domain/Speler.java
@@ -0,0 +1,50 @@
+package domain;
+
+public class Speler {
+	private String naam;
+	private int score;
+	
+	
+	public Speler(String naam) {
+		if (naam == null || naam.trim().isEmpty()) throw new DomainException("naam mag niet null zijn");
+		this.naam = naam;
+		this.score = 0;
+	}
+
+
+	public String getNaam() {
+		return naam;
+	}
+
+
+	public int getScore() {
+		return score;
+	}
+	
+	public void addToScore(int score){
+		
+		this.score += score;
+		if (this.score < 0) {
+			this.score-=score;
+			throw new DomainException("score mag niet 0 zijn");
+			
+		}
+		
+		
+		
+	}
+	
+	public boolean equals(Object object){
+		if (object instanceof Speler){
+			Speler s = (Speler) object;
+			if (s.getNaam().equals(this.getNaam()) && s.getScore() == this.getScore())
+				return true;
+		}
+		return false;
+	}
+	
+	public String toString(){
+		return this.naam + " score : " + this.score;
+	}
+
+}
