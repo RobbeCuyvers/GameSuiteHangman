3520093efc0f0d57b9b3474b266fe9ae55dad231
DESKTOP-57INN8U\Michi <michieldewandelaer@hotmail.com>
2017-05-08 15:58:26 +0200

    launcher

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -3,15 +3,26 @@ package ui;
 import javax.swing.JOptionPane;
 import domain.*;
 
-import domain.*;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		Speler speler = new Speler(naam);
-
+		Speler speler =null;
+		try {
+		 speler = new Speler(naam);
+		}
+		catch (DomainException e){
+			JOptionPane.showMessageDialog(null, e.getMessage(),null, JOptionPane.ERROR_MESSAGE);
+			
+		}
+		
 		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		
+		Object[] shapes = {"Cirkel", "Rechthoek"};
+		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen", "input", JOptionPane.INFORMATION_MESSAGE, null, shapes, null);
+		if (keuze.equals("Cirkel")){
+			
 		
 		
 		int x = Integer.parseInt(JOptionPane.showInputDialog("x-coordinaat van het middelpunt:"));
@@ -19,7 +30,22 @@ public class Launcher {
 		int radius = Integer.parseInt(JOptionPane.showInputDialog("Radius van de cirkel:"));
 		Punt punt = new Punt(x,y);
 		Cirkel cirkel = new Cirkel(punt, radius);
+		
 		JOptionPane.showMessageDialog(null, "U heeft een correcte cirkel aangemaakt: "+cirkel.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 	}
+		if (keuze.equals("Rechthoek")){
+			int x = Integer.parseInt(JOptionPane.showInputDialog("x-coordinaat van linkerbovenhoek:"));
+			int y = Integer.parseInt(JOptionPane.showInputDialog("y-coordinaat van linkerbovenhoek:"));
+			
+			Punt punt = new Punt(x,y);
+			int breedte = Integer.parseInt(JOptionPane.showInputDialog("x-coordinaat van breedte:"));
+			int hoogte = Integer.parseInt(JOptionPane.showInputDialog("y-coordinaat van hoogte:"));
+			Rechthoek rechthoek = new Rechthoek(punt, breedte, hoogte);
+			
+			JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek aangemaakt: "+ rechthoek.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+			
+		}
+	}
+	
 
 }
