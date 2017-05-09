90ce55386ce3aff474625c6c0c0cc4cfaf078496
DESKTOP-57INN8U\Michi <michieldewandelaer@hotmail.com>
2017-05-09 10:26:47 +0200

    launcher update

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -9,21 +9,32 @@ public class Launcher {
 	public static void main(String[] args) {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
 		Speler speler =null;
+		boolean lus = true;
+		do {
+			
 		try {
 		 speler = new Speler(naam);
+		 lus = false;
 		}
 		catch (DomainException e){
 			JOptionPane.showMessageDialog(null, e.getMessage(),null, JOptionPane.ERROR_MESSAGE);
 			
 		}
+		}
+		while(lus);
+		lus = true;
 		
 		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 		
 		Object[] shapes = {"Cirkel", "Rechthoek"};
-		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen", "input", JOptionPane.INFORMATION_MESSAGE, null, shapes, null);
-		if (keuze.equals("Cirkel")){
-			
 		
+		
+		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen", "input", JOptionPane.INFORMATION_MESSAGE, null, shapes, null);;
+		
+		do {
+			
+			try {
+		if (keuze.equals("Cirkel")){
 		
 		int x = Integer.parseInt(JOptionPane.showInputDialog("x-coordinaat van het middelpunt:"));
 		int y = Integer.parseInt(JOptionPane.showInputDialog("y-coordinaat van het middelpunt:"));
@@ -45,7 +56,20 @@ public class Launcher {
 			JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek aangemaakt: "+ rechthoek.toString(), speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 			
 		}
-	}
-	
+		
+		lus = false;
+			}
+			catch (DomainException e){
+				JOptionPane.showMessageDialog(null, e.getMessage(),null, JOptionPane.ERROR_MESSAGE);
+				
+			}
+			catch (NumberFormatException e){
+				JOptionPane.showMessageDialog(null, "Gelieve een geldig getal in te vullen.",null, JOptionPane.ERROR_MESSAGE);
+			}
+			}
+			while(lus);
+			lus = true;
+			
 
+}
 }
