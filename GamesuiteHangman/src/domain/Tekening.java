1496e13fcc7868691b6ba49d78a6c5be952a5ef5
DESKTOP-57INN8U\Michi <michieldewandelaer@hotmail.com>
2017-05-09 11:55:51 +0200

    commit Tekening niet af ( Denker Rémi & Doener Michiel)

--- GamesuiteHangman/src/domain/Tekening.java
+++ GamesuiteHangman/src/domain/Tekening.java
@@ -1,19 +1,69 @@
 package domain;
 
 import java.util.*;
+import domain.Omhullende;
 
 public class Tekening {
 	private String naam;
-	private int MIN_X, MIN_Y,MAX_X,MAX_Y;
+	private static final int MIN_X = 0, MIN_Y = 0,MAX_X = 399 ,MAX_Y = 399;
 	private List vormen;
 	
 	public Tekening(String naam){
-		if (naam == null || naam.trim().isEmpty()) throw new DomainException("naam mag niet leeg zijn.");
+		if (naam == null || naam.trim().isEmpty()) throw new IllegalArgumentException("naam mag niet leeg zijn.");
 		this.naam = naam;
 		vormen = new ArrayList();
 		
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public int getVorm(int vorm) {
+		return vorm;
+	}
+	
+	public void voegToe(Vorm vorm){
+		Tekening tekening1 = new Tekening("vorm");
+		//if (MAX_X - MIN_X == Omhullende)
 		
 	}
+	
+	public int getAantalVormen(){
+		int aantalvormen = 0;
+		for (int i = 0; vormen.size() > i; i++ ){
+			aantalvormen =+ 1;
+		}
+			
+		return aantalvormen;
+	}
+	
+	public void verwijder(Vorm vorm){
+		
+	}
+	
+	public boolean bevat(Vorm vorm){
+		return true;
+		
+	}
+	
+	public String toString(){
+		return null;
+	}
+	
+	
+	
+	@Override
+	public boolean equals(Object object){
+		if (object instanceof Tekening){
+			Tekening s = (Tekening) object;
+			if ()
+				return true;
+		}
+		return false;
+	}
+	
+	
 	
 	
 
