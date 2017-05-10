aa7d12476173f387ecc430960477b07f8a882ee0
Kenneth Baksteen <kenneth.baksteen@student.ucll.be>
2017-05-10 13:43:54 +0200

    kleine aanpassing

--- GamesuiteHangman/src/domain/WoordenLijst.java
+++ GamesuiteHangman/src/domain/WoordenLijst.java
@@ -2,10 +2,6 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.nio.file.Paths;
-import java.util.*;
 
 public class WoordenLijst {
 	private List<String> woorden;
@@ -13,15 +9,11 @@ public class WoordenLijst {
 	public WoordenLijst() {
 		woorden = new ArrayList<String>();
 		// Inlezen van bestand hangman.txt
-
 	}
 
 	public int getAantalWoorden() {
 		return woorden.size();
 	}
-
-
-
 
 	public void voegToe(String woord){
 		if(woord==null||woord.trim().isEmpty()||woorden.contains(woord))
@@ -31,7 +23,6 @@ public class WoordenLijst {
 
 	public String getRandomWoord() {
 		int index = (int) Math.floor(Math.random() * (woorden.size() - 1));
-
 		return woorden.get(index);
 	}
 }
